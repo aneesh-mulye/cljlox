133d95baae9f8c908de8524384beef2db177e1eb
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2024-08-15 04:30:51 +0530

    Correctly configure Conjure with CIDER

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cider/cider-nrepl "0.49.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :plugins [[cider/cider-nrepl "0.49.2"]]
   :main ^:skip-aot cljlox.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
